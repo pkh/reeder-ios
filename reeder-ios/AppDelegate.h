25a899c21cdb319e0144eec3a8fda0f99c05fc61
Patrick Hanlon <me@patrickhanlon.org>
2013-06-23 16:14:35 -0500

    Initial Commit

---
+++ reeder-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  reeder-ios
+//
+//  Created by Patrick Hanlon on 6/23/13.
+//  Copyright (c) 2013 pkh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ reeder-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  reeder-ios
+//
+//  Created by Patrick Hanlon on 6/23/13.
+//  Copyright (c) 2013 pkh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
